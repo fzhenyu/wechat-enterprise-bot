ebdf4d18d7cae91eb6670e65e8941381639fbeaf
Ji Wu <ji.wu@outlook.com>
2016-02-20 22:38:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+WeChat Enterprise Bot
+==============================
+
+> A silly parrot bot that repeats user input for WeChat Enterprise account powered by Python (2.7) Flask
+
+
+## Get Started
+
+Step 1. Clone this repo
+```bash
+$ git clone https://github.com/j1wu/wechat-enterprise-bot.git
+```
+Step 2. Add your wechat enterprise app keys to `app.py` (replace `None`)
+
+![](https://cloud.githubusercontent.com/assets/5327840/13209245/bb2354e6-d95c-11e5-8197-e9c3b66be42a.png)
+
+Step 3. Install virtualenv
+```bash
+$ sudo easy_install virtualenv
+```
+Step 4. Create a new virtual environment
+```bash
+$ virtualenv venv
+```
+Step 5. Activate the new virtual environment
+```bash
+$ source venv/bin/activate
+```
+Step 6. Install app dependencies within the virtual environment
+```bash
+(venv) $ cd wechat-enterprise-bot
+(venv) $ pip install -r requirements.txt
+```
+Step 7. Start app
+```bash
+(venv) $ python app.py
+```
+
+
+## Host The Bot Locally
+
+Step 1. Install [ngrok](https://github.com/inconshreveable/ngrok)
+
+Step 2. Create an account on [ngrok.com](https://ngrok.com/signup)
+
+Step 3. Install ngrok authtoken
+```bash
+./ngrok authtoken your_authentication_key
+```
+Step 4. Start Flask app first
+```bash
+(venv) $ python app.py
+```
+Step 5. Start ngrok
+```bash
+$ ./ngrok http 8080
+```
+Step 6. Configure your wechat app with the ngrok fowarding url
+
+![](https://cloud.githubusercontent.com/assets/5327840/13206716/0e3de920-d93f-11e5-97b8-0773d1656ae9.png)
+
+![](https://cloud.githubusercontent.com/assets/5327840/13206743/7b4ba8f4-d93f-11e5-8d1a-0cf99712b043.png)
+
+Step 7. Verify it
+
+Now you'll be able to see the traffic coming from the wechat server :)
+
+![](https://cloud.githubusercontent.com/assets/5327840/13206782/3b14a60e-d940-11e5-9c8d-0acef9a1646e.png)
+
+
+## Host The Bot with Heroku
+
+Step 1. Install [heroku toolbelt](https://toolbelt.heroku.com/)
+
+Step 2. Create an account on [heroku](https://signup.heroku.com/)
+
+Step 3. Create a new heroku app
+```bash
+$ heroku login
+$ heroku keys:add
+$ heroku apps:create your_app_name
+``` 
+Step 4. Install [foreman](https://github.com/ddollar/foreman)
+```bash
+$ gem install foreman
+```
+Step 5. Local test run with foreman
+```bash
+# make sure to activate the virtual environment first
+(venv) $ foreman start
+```
+Step 6. Deploy to heroku
+```bash
+(venv) $ git add .
+(venv) $ git commit -m "initial deployment"
+(venv) $ git push heroku master
+```
+Step 7. Update your wechat app configuration with the heroku app url
---
+++ app.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+import base64
+import hashlib
+import random
+import socket
+import string
+import struct
+import time
+import xml.etree.cElementTree as ET
+
+from Crypto.Cipher import AES
+
+from flask import Flask, make_response, render_template, request
+
+from pkcs7 import PKCS7Encoder
+
+
+app = Flask(__name__)
+
+WECHAT_TOKEN = None
+WECHAT_ENCODINGAESKEY = None
+WECHAT_CORPID = None
+
+app.config.from_object(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if not request.args:
+        return 'Hello WeChat'
+    else:
+        msg_signature = request.args.get('msg_signature')
+        timestamp = request.args.get('timestamp')
+        nonce = request.args.get('nonce')
+
+    # validate the server
+    if request.method == 'GET':
+        echostr = request.args.get('echostr')
+        decrypted_echostr = decrypt_message(msg_signature=msg_signature,
+                                            timestamp=timestamp, nonce=nonce,
+                                            echostr=echostr)
+        if decrypted_echostr:
+            return decrypted_echostr
+
+    # handle coming messages
+    elif request.method == 'POST':
+        data = request.data
+        xmlstr_received = ET.fromstring(data)
+        message_received = parse_message(xmlstr=xmlstr_received)
+        encrypted_message = message_received['Encrypt']
+        decrypted_message = decrypt_message(msg_signature=msg_signature,
+                                            timestamp=timestamp, nonce=nonce,
+                                            msg_encrypt=encrypted_message)
+        try:
+            decrypted_xml = ET.fromstring(decrypted_message)
+            message_dict = parse_message(xmlstr=decrypted_xml)
+            user_input = get_user_input(message_dict)
+        except Exception as e:
+            app.logger.error('parse decrypted message error: ' + str(e))
+
+        message_dict['Time'] = str(int(time.time()))
+        message_dict['Content'] = generate_reply(user_input)
+        message_body = render_template('message.xml', message=message_dict)
+
+        encrypted_message = encrypt_message(message_body)
+        signature_list = [app.config['WECHAT_TOKEN'], timestamp,
+                          nonce, encrypted_message]
+        message_signature = generate_signature(signature_list)
+
+        template = render_template('response.xml', message=encrypted_message,
+                                   signature=message_signature,
+                                   timestamp=timestamp, nonce=nonce)
+        response = make_response(template)
+        response.headers['Content-Type'] = 'application/xml'
+        return response
+
+
+def get_user_input(message):
+    return message['Content']
+
+
+def generate_reply(user_input):
+    return user_input
+
+
+def parse_message(xmlstr):
+    message = {}
+    if xmlstr.tag == 'xml':
+        for child in xmlstr:
+            message[child.tag] = child.text
+    return message
+
+
+def decrypt_message(**kwargs):
+    msg_signature = kwargs.get('msg_signature')
+    timestamp = kwargs.get('timestamp')
+    nonce = kwargs.get('nonce')
+
+    echostr = kwargs.get('echostr')
+    msg_encrypt = kwargs.get('msg_encrypt')
+    msg_content = echostr or msg_encrypt
+    token = app.config['WECHAT_TOKEN']
+
+    signature_list = [token, timestamp, nonce, msg_content]
+    raw_signature = generate_signature(signature_list)
+    if raw_signature != msg_signature:
+        app.logger.error('error: signatures do not match')
+        return None
+    else:
+        try:
+            plain_text = decrypt_plain_text(msg_content)
+        except Exception as e:
+            app.logger.error('decrypt plaint text error: ' + str(e))
+            return None
+        try:
+            xml_content = decrypt_xml_content(plain_text)
+            return xml_content
+        except Exception as e:
+            app.logger.error('decrypt xml content error: ' + str(e))
+            return None
+
+
+def decrypt_plain_text(msg_content):
+    key = base64.b64decode(app.config['WECHAT_ENCODINGAESKEY'] + '=')
+    cryptor = AES.new(key, AES.MODE_CBC, key[:16])
+    plain_text = cryptor.decrypt(base64.b64decode(msg_content))
+    return plain_text
+
+
+def decrypt_xml_content(plain_text):
+    pad = ord(plain_text[-1])
+    content = plain_text[16:-pad]
+    xml_len = socket.ntohl(struct.unpack('I', content[:4])[0])
+    xml_content = content[4: xml_len + 4]
+    server_corpid = content[xml_len + 4:]
+    local_corpid = app.config['WECHAT_CORPID']
+    if server_corpid != local_corpid:
+        app.logger.error('error: corporate IDs do not match')
+        return None
+    return xml_content
+
+
+def generate_signature(signature_list):
+    signature_list.sort()
+    sha = hashlib.sha1()
+    sha.update(''.join(signature_list))
+    signature = sha.hexdigest()
+    return signature
+
+
+def encrypt_message(message):
+    key = base64.b64decode(app.config['WECHAT_ENCODINGAESKEY'] + '=')
+    random_string = generate_random_string(16)
+    local_corpid = app.config['WECHAT_CORPID']
+    try:
+        packed_binary = struct.pack('I', socket.htonl(len(message)))
+        message = random_string + packed_binary + message + local_corpid
+    except Exception as e:
+        app.logger.error('encrypt message error: ' + str(e))
+        return None
+    pkcs7 = PKCS7Encoder()
+    message = pkcs7.encode(message)
+    cryptor = AES.new(key, AES.MODE_CBC, key[:16])
+    try:
+        ciphertext = cryptor.encrypt(message)
+        base64text = base64.b64encode(ciphertext)
+        return base64text
+    except Exception as e:
+        app.logger.error('encrypt message error: ' + str(e))
+        return None
+
+
+def generate_random_string(digits):
+    source = string.ascii_uppercase + string.digits
+    random_string = ''.join(random.choice(source) for _ in range(digits))
+    return random_string
+
+
+def detect_configuration():
+    token = app.config['WECHAT_TOKEN']
+    aeskey = app.config['WECHAT_ENCODINGAESKEY']
+    corpid = app.config['WECHAT_CORPID']
+
+    if (token and aeskey and corpid) is None:
+        raise Exception('missing wechat configuration!')
+
+
+if __name__ == '__main__':
+    detect_configuration()
+    app.run(host='0.0.0.0', port=8080, debug=True)
